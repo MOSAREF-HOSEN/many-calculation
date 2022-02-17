0be797ae1fb6f230eaab2ff2f390498ea6520766
Golap-mostofa <mostofa6410@gmail.com>
2022-02-17 15:15:23 +0600

    total

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
 
 
 
-    const totalAmount = document.getElementById('total-amount');
-    const shopAmount = document.getElementById('shop');
-    const grocarAmount = document.getElementById('grocar');
-    const rentAmount = document.getElementById('rent');
-    const totalprice = document.getElementById('total');
+    // let totalAmount = document.getElementById('total-amount');
+    // let shopAmount = document.getElementById('shop');
+    // let grocarAmount = document.getElementById('grocar');
+    // let rentAmount = document.getElementById('rent');
+    // let totalprice = document.getElementById('total');
     
-function hendelar(){
+// function hendelar(){
     //update shoping parice 
-    const shopValue = shopAmount.value;
-    const grocarValue = grocarAmount.value;
-    const rentValue = rentAmount.value;
-    totalprice.innerText = parseInt(shopValue)+parseInt(grocarValue)+parseInt(rentValue);
-    console.log(totalprice);
+    // let shopValue = shopAmount.value;
+    // let grocarValue = grocarAmount.value;
+    // let rentValue = rentAmount.value;
+    // totalprice.innerText = parseInt(shopValue)+parseInt(grocarValue)+parseInt(rentValue);
+    // console.log(totalprice);
 
 //update main total
+// let totalAmountValue = totalAmount.value;
+// }
+
+document.getElementById('calculetar').addEventListener('click',function(){
+    
+    let shopAmount = document.getElementById('shop');
+    let shopValue = shopAmount.value;
+    let grocarAmount = document.getElementById('grocar');
+    let grocarValue = grocarAmount.value;
+    let rentAmount = document.getElementById('rent');
+    let rentValue = rentAmount.value;
+    let totalprice = document.getElementById('total');
+   
+    totalprice.innerText = parseInt(shopValue)+
+     parseInt(grocarValue)+parseInt(rentValue);
+
+//update total
+    let totalAmount = document.getElementById('total-amount');
+    let totalAmountValu = parseInt(totalAmount.value)-parseInt(totalprice.innerText);
+
+    let balenceTotal = document.getElementById('balenc');
+     balenceTotal.innerText = totalAmountValu;
+     console.log(balenceTotal);
 
 
+    
 
 
-}
+     //clear input filt
+     shopAmount.value = '';
+     grocarAmount.value = '';
+     rentAmount.value = '';
+     totalAmount.value = '';
+     
+     
+})
